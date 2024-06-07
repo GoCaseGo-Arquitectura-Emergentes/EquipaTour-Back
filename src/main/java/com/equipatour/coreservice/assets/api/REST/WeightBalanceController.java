40bba5d7d9ed0081e8b413ceb2809a9544c5f3f1
NicolePT <u201921559@upc.edu.pe>
2024-06-07 15:34:32 -0500

    feat: added cors configuration

--- src/main/java/com/equipatour/coreservice/assets/api/REST/WeightBalanceController.java
+++ src/main/java/com/equipatour/coreservice/assets/api/REST/WeightBalanceController.java
@@ -23,7 +23,6 @@ import static com.equipatour.coreservice.shared.messages.ConfigurationMessages.W
 @RestController
 @RequestMapping("/api/v1/weight-balances")
 @Tag(name="Weight Sensors Controller")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class WeightBalanceController {
 
     private final WeightBalanceCommandService weightBalanceCommandService;
