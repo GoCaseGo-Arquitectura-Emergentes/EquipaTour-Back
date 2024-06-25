d047c08568313a322b0c252c3fe26dddb815bc28
Djalma Dioses <54141599+djalmadioses@users.noreply.github.com>
2024-06-25 15:49:44 -0500

    fix: CORS policity

---
+++ src/main/java/com/equipatour/coreservice/shared/infrastructure/authorization/sfs/configuration/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.equipatour.coreservice.shared.infrastructure.authorization.sfs.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Allow your frontend domain
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD") // Specify allowed methods
+                        .allowedHeaders("*") // Allow any headers
+                        .allowCredentials(true); // Allow credentials
+            }
+        };
+    }
+}
